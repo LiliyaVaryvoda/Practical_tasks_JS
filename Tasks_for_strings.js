f8c2ac6e14d6bd6493a1b7e30478374d6ec8e148
Liliia Varyvoda <liliia.varyvoda@wpp.com>
2025-10-27 15:16:50 +0200

    tasks/Tasks_for_strings_2

--- Tasks_for_strings.js
+++ Tasks_for_strings.js
@@ -3,10 +3,60 @@
 
 
 function isPalindrome(str) {
-    let reversedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '').split('').reverse().join('')
-    return str === reversedStr
+    let cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '')
+    let reversedStr = cleanedStr.split('').reverse().join('')
+    return cleanedStr === reversedStr
 }
 
 
 console.log(isPalindrome("racecar")); // true
 console.log(isPalindrome("hello world")); // false
+console.log(isPalindrome("A man, a plan, a canal: Panama")) // true
+
+
+
+
+
+
+// 2. Find occurrences in the string for letters
+
+
+function findOccurrencesStr(str){
+    let mapOccurencies = new Map()
+
+    for (let i = 0; i<str.length; i++){
+        if (mapOccurencies.has(str[i])){
+            mapOccurencies.set(str[i], mapOccurencies.get(str[i])+1)
+        }
+        else{
+            mapOccurencies.set(str[i], 1)
+        }
+    }
+    return mapOccurencies
+}
+
+
+console.log(findOccurrencesStr("prgogrammingg"))
+
+
+// 3. Find occurrences in the string for words
+
+
+function findOccurrencesWords(str){
+    let mapOccurencies = new Map()
+    let arrayWords = str.toLowerCase().replace(/[^\w\s]/g, '')
+    .split(/\s+/)
+
+    for (let i = 0; i<arrayWords.length; i++){
+        if (mapOccurencies.has(arrayWords[i])){
+            mapOccurencies.set(arrayWords[i], mapOccurencies.get(arrayWords[i])+1)
+        }
+        else{
+            mapOccurencies.set(arrayWords[i], 1)
+        }
+    }
+    return mapOccurencies
+}
+
+
+console.log(findOccurrencesWords("hello world how are you hello, doing hello are ok"))
