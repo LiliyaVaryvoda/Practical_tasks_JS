1cedad13a1fbae38646fdd006f21a1704f8befad
Liliia Varyvoda <liliia.varyvoda@wpp.com>
2025-11-17 19:15:44 +0200

    tasks/Tasks/1

--- Tasks_for_arrays/Tasks_for_arrays.js
+++ Tasks_for_arrays/Tasks_for_arrays.js
@@ -1,14 +1,14 @@
 // 1. CALCULATE THE SUM OF EACH VALUE OF TWO ARRAYS AND ADD THEM TO THIRD ARRAY
 
-function calculateArraysSum(arr1, arr2){
+function calculateArraysSum(arr1, arr2) {
     let arraySumResult = []
     const maxValue = Math.max(arr1.length, arr2.length)
 
-    for (let i = 0; i < maxValue; i++){
+    for (let i = 0; i < maxValue; i++) {
         let arr1Value = arr1[i] ?? 0
         let arr2Value = arr2[i] ?? 0
 
-        arraySumResult.push(arr1Value+arr2Value)
+        arraySumResult.push(arr1Value + arr2Value)
     }
     return arraySumResult
 }
@@ -25,12 +25,12 @@ console.log(calculateArraysSum([2, 4, 6, 8, 0], [1, 3, 5, 7, 9, 11, 13]))
 
 // 2. FLAT AN ARRAY TO REPEAT ALL ELEMENTS
 
-function arrayFlatMap(arr){
+function arrayFlatMap(arr) {
     const finalArray = arr.flatMap(elem => new Array(elem).fill(elem))
     return finalArray
 }
 
-console.log(arrayFlatMap([7,0,1]))
+console.log(arrayFlatMap([7, 0, 1]))
 //[7,7,7,7,7,7,7,1]
 
 
@@ -38,7 +38,7 @@ console.log(arrayFlatMap([7,0,1]))
 
 
 // 3. FLAT NESTED ARRAY
-function flatNestedArray(arr){
+function flatNestedArray(arr) {
     const finalArray = arr.flat(Infinity)
     return finalArray
 }
@@ -52,11 +52,11 @@ console.log(flatNestedArray([1, 2, [3, 4, [5, 6]]]))
 
 // 4. FIND MAX VALUE OF ARRAY ELEMENTS
 
-function arrMaxNum(arr){
+function arrMaxNum(arr) {
     const maxValue = Math.max(...arr)
     return maxValue
 }
-console.log(arrMaxNum([6,8,9,4]))
+console.log(arrMaxNum([6, 8, 9, 4]))
 // 9
 
 
@@ -65,9 +65,9 @@ console.log(arrMaxNum([6,8,9,4]))
 
 // 5. FIND TWO SMALLEST NUMBERS IN ARRAY AND ARRAY SHOUDL BE UNCHANGED
 
-function findSmallest(arr){
-    const arrSorted = [...arr].sort((a,b) => a-b)
-    return arrSorted.slice(0,2)
+function findSmallest(arr) {
+    const arrSorted = [...arr].sort((a, b) => a - b)
+    return arrSorted.slice(0, 2)
 }
 
 console.log(findSmallest([6, 4, -6, 8, 2, 1, 5])) //[-6, 1]
@@ -81,11 +81,11 @@ console.log(findSmallest([6])) // [6]
 
 // 6. FIND LAST ELEMENT OF ARRAY
 
-function findLastArrElem(arr){
-    return arr[arr.length-1]
+function findLastArrElem(arr) {
+    return arr[arr.length - 1]
 }
 
-console.log(findLastArrElem([6,4,3,2]))   //2
+console.log(findLastArrElem([6, 4, 3, 2]))   //2
 console.log(findLastArrElem(['hello', 'world', '!']))  //!
 
 
@@ -94,13 +94,13 @@ console.log(findLastArrElem(['hello', 'world', '!']))  //!
 
 // 7. FIND LARGEST ELEMENT IN NESTED ARRAY
 
-function findLargestNested(arr){
+function findLargestNested(arr) {
     const arrFlat = arr.flat(Infinity)
     const maxValue = Math.max(...arrFlat)
     return maxValue
 }
 
-console.log(findLargestNested([1, [2, 3], [4, [5, 6]], 7, [8, 9]]))
+console.log(findLargestNested([1, [2, 3], [4, [5, 6]], 7, [8, 9]]))  // 9
 
 
 
@@ -108,10 +108,10 @@ console.log(findLargestNested([1, [2, 3], [4, [5, 6]], 7, [8, 9]]))
 
 // 8. REMOVE ALL FALSY VALUES FROM ARRAY
 
-function removeFalsyValuesVar1(arr){
-    const updatedArr=[]
-    for (let elem of arr){
-        if (Boolean(elem)){
+function removeFalsyValuesVar1(arr) {
+    const updatedArr = []
+    for (let elem of arr) {
+        if (Boolean(elem)) {
             updatedArr.push(elem)
 
         }
@@ -128,8 +128,8 @@ console.log(removeFalsyValuesVar1([7, "ate", "", false, 9])); // [7, "ate", 9]
 
 // 9. REMOVE ALL FALSY VALUES FROM ARRAY VARIANT 2
 
-function removeFalsyValuesVar2(arr){
-    const updatedArr= arr.filter(Boolean)
+function removeFalsyValuesVar2(arr) {
+    const updatedArr = arr.filter(Boolean)
     //const updatedArr= arr.filter(elem => Boolean(elem))
     return updatedArr
 }
@@ -152,7 +152,7 @@ function alternateMerge(arr1, arr2) {
         if (i < arr1.length) {
             arr3.push(arr1[i])
         }
-        if (i < arr2.length){
+        if (i < arr2.length) {
             arr3.push(arr2[i])
         }
     }
@@ -176,21 +176,21 @@ console.log(alternateMerge([10, ""], ['a', 'b', false, 'd']))
 
 // 11.  GROUP EVEN AND ODDS ELEMENTS FROM ARRAY INTO SEPARATE ARRAYS
 
-function groupArrayNumbers(arr){
+function groupArrayNumbers(arr) {
     const arrOdd = []
     const arrEven = []
-    for (let char of arr){
-        if (char%2 === 0){
+    for (let char of arr) {
+        if (char % 2 === 0) {
             arrEven.push(char)
         }
-        else{
+        else {
             arrOdd.push(char)
         }
     }
-    return{arrOdd, arrEven}
+    return { arrOdd, arrEven }
 }
 
-console.log(groupArrayNumbers([5,2,8,7,9,3,2,6]))
+console.log(groupArrayNumbers([5, 2, 8, 7, 9, 3, 2, 6]))
 // { arrOdd: [ 5, 7, 9, 3 ], arrEven: [ 2, 8, 2, 6 ] }
 
 
@@ -200,11 +200,11 @@ console.log(groupArrayNumbers([5,2,8,7,9,3,2,6]))
 
 // 12. RETURN ARRAY LONGEST STRING ELEMENT
 
-function arrLongestStr(arr){
+function arrLongestStr(arr) {
     let maxValue = 0
     let updatedValue = ''
-    for (let char of arr){
-        if (char.length > maxValue){
+    for (let char of arr) {
+        if (char.length > maxValue) {
             maxValue = char.length
             updatedValue = char
         }
@@ -222,8 +222,8 @@ console.log(arrLongestStr(["apple", "banana", "cherry", "date", "elderberry"]))
 
 // 13. RETURN ARRAY LONGEST STRING ELEMENT VARIANT 2
 
-function arrLongestStrVar2(arr){
-    const arr2 = arr.reduce((longest, current) => { return longest.length>current.length ? longest : current})
+function arrLongestStrVar2(arr) {
+    const arr2 = arr.reduce((longest, current) => { return longest.length > current.length ? longest : current })
     return arr2
 }
 
@@ -237,7 +237,7 @@ console.log(arrLongestStrVar2(["apple", "banana", "cherry", "date", "elderberry"
 
 // 14. FIND COMMON DIGITS FROM TWO ARRAYS
 
-function findCommon(arr1, arr2){
+function findCommon(arr1, arr2) {
     const arr3 = arr1.filter(elem => arr2.includes(elem))
     return [...new Set(arr3)]
 }
@@ -252,7 +252,7 @@ console.log(findCommon([1, 2, 3, 4, 6, 6], [3, 4, 5, 6]))
 
 // 15. RETURN UNION OF TWO ARRAYS WITH UNIQUE ELEMENTS
 
-function returnUnion(arr1, arr2){
+function returnUnion(arr1, arr2) {
     const arr3 = new Set([...arr1, ...arr2])
     return [...arr3]
 }
@@ -267,8 +267,8 @@ console.log(returnUnion([1, 2, 3, 4, 6], [3, 4, 5, 6]))
 
 // 16. RETURN SUM OF NUMBERS OF ARRAY
 
-function returnArrSum(arr){
-return arr.reduce((acc, current) => {return acc + current})
+function returnArrSum(arr) {
+    return arr.reduce((acc, current) => { return acc + current })
 }
 
 console.log(returnArrSum([1, 2, 3, 4, 5])) //15
@@ -279,8 +279,8 @@ console.log(returnArrSum([1, 2, 3, 4, 5])) //15
 
 // 17. CHECK IF ARRAY IS SORTED
 
-function checkArrSorted(arr){
-    const arrSorted = [...arr].sort((a,b) => a-b)
+function checkArrSorted(arr) {
+    const arrSorted = [...arr].sort((a, b) => a - b)
     console.log(arrSorted)
     console.log('arr', arr)
     return arr.every((value, index) => value === arrSorted[index])
@@ -298,9 +298,9 @@ console.log(checkArrSorted([10, 2, 30])) // false
 
 // 18. SORT ARRAY OF OBJECTS BY PRICE FROM MAX TO MIN
 
-function sortArrayOfObjects(arr){
-    
-    const sortedArray = [...arr].sort((a,b) =>  b.price-a.price)
+function sortArrayOfObjects(arr) {
+
+    const sortedArray = [...arr].sort((a, b) => b.price - a.price)
     return sortedArray
 }
 
@@ -308,8 +308,15 @@ console.log(sortArrayOfObjects([
     { name: "item1", price: 10 },
     { name: "item2", price: 5 },
     { name: "item3", price: 20 }
-  ]
- ))
+]
+))
+
+
+//  [
+//     { name: 'item3', price: 20 },
+//     { name: 'item1', price: 10 },
+//     { name: 'item2', price: 5 }
+//   ]
 
 
 
@@ -318,17 +325,36 @@ console.log(sortArrayOfObjects([
 
 
 
- // 19. RETURN PROPERTIES OF ARRAY OBJECTS BASED ON CONDITION: RETURN NAMES THAT ARE OLDER THAN SOME AGE
+// 19. RETURN PROPERTIES OF ARRAY OBJECTS BASED ON CONDITION: RETURN NAMES THAT ARE OLDER THAN SOME AGE
 
- function returnUserNames(arr){
-    const filteredValues =  arr.filter(elem => elem.age > 30
+function returnUserNames(arr) {
+    const filteredValues = arr.filter(elem => elem.age > 30
     ).map(elem => elem.name)
     return filteredValues
- }
+}
 
- console.log(returnUserNames([
+console.log(returnUserNames([
     { name: "Alice", age: 25 },
     { name: "Bob", age: 35 },
     { name: "John", age: 45 }
-  ]
- ))
+]
+))
+
+// [ 'Bob', 'John' ]
+
+
+
+
+
+
+// 20. RETURN AVERAGE VALUE FROM ARRAY
+
+function returnAverageValue(arr) {
+    if (arr.length === 0) return 0;
+    const arrSum = arr.reduce((acc, current) => acc + current)
+    return Math.round(arrSum / arr.length)
+}
+
+console.log(returnAverageValue([6, 5, 8, 3, 5, 7])) //6
+console.log(returnAverageValue([0])) //0
+console.log(returnAverageValue([1])) //1
